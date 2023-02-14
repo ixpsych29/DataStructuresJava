b7b3caacb415072ef204573e13eb03900b6e8207
Abdullah Bin Rafique <abdullahbinrafique945@gmail.com>
2023-02-14 10:38:00 +0500

    2D array

--- Arrays/array_2d.java
+++ Arrays/array_2d.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
+
 public class array_2d {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        int row, col;
+        try (Scanner sc = new Scanner(System.in)) {
+            // taking input
+            System.out.println("Enter the number of rows");
+            row = sc.nextInt();
+            System.out.println("Enter the number of columns");
+            col = sc.nextInt();
 
+            // making a 2D array
+            System.out.println("A matrix of " + row + " x " + col + " is created successfully!");
+            int arr[][] = new int[row][col];
 
-    }//end main
-}//end class
+            // taking values for the matrix
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    System.out.println("Enter value for [" + i + "][" + j + "]");
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+
+            // reading values from the array
+            System.out.println("Values are: ");
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+    }// end main
+}// end class
